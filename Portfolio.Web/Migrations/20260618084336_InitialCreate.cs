a1eae91025155bbf90e0ca822c8e9fe593791386
Mohammadreza Arab <mohammadreza.arab.de@gmail.com>
2026-06-18 12:17:12 +0330

    feat: add initial database migration

---
+++ Portfolio.Web/Migrations/20260618084336_InitialCreate.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Portfolio.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "certificate",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Issuer = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Year = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ImageName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_certificate", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "educations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Year = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_educations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "experiences",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Year = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    CompanyName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    CompanyUrl = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    WorkInfo = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_experiences", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "homePageSettings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HeroTitle = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    HeroName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    HeroDescription = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false),
+                    HeroImageName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HeroLocation = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    HeroStatus = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    HeroFocus = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    HeroLanguage = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    AboutTitle = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    AboutSummary = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    AboutDescription = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    TitleContact = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmailUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GithubUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LinkedinUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MetaTitle = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    MetaDescription = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_homePageSettings", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LearningItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LearningItems", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "skillCategorys",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_skillCategorys", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "skill",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SkillsTitle = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    SkillCategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_skill", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_skill_skillCategorys_SkillCategoryId",
+                        column: x => x.SkillCategoryId,
+                        principalTable: "skillCategorys",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_skill_SkillCategoryId",
+                table: "skill",
+                column: "SkillCategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "certificate");
+
+            migrationBuilder.DropTable(
+                name: "educations");
+
+            migrationBuilder.DropTable(
+                name: "experiences");
+
+            migrationBuilder.DropTable(
+                name: "homePageSettings");
+
+            migrationBuilder.DropTable(
+                name: "LearningItems");
+
+            migrationBuilder.DropTable(
+                name: "skill");
+
+            migrationBuilder.DropTable(
+                name: "skillCategorys");
+        }
+    }
+}
